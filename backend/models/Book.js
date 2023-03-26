094fa89f17ae5ed4d0d8087d1167da4b9de3f3ae
gurcancekic <arinyazilim@gmail.com>
2023-03-26 23:04:09 +0300

    book and user model initial code

---
+++ backend/models/Book.js
@@ -0,0 +1,99 @@
+import mongoose from 'mongoose';
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    authors: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    description: {
+      type: String,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    pageNumber: {
+      type: Number,
+      min: 1,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Book', bookSchema);
+
+/* const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  authors: [{
+    type: String,
+    required: true,
+  }],
+  description: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  status: {
+    type: String,
+    enum: ['pending', 'approved', 'rejected'],
+    default: 'pending',
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  comments: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    likes: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    }],
+    dislikes: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    }],
+  }],
+  ratings: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    value: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+  }],
+}, { timestamps: true });
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book; */
