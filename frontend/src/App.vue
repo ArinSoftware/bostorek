5aa3007bc91c25f39eeb071ad49887206e6a9d59
gurcancekic <arinyazilim@gmail.com>
2023-04-14 22:29:27 +0300

    Commentlist.vue initial code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,17 +2,19 @@
   <main-menu />
   <the-hero />
   <book-list />
+  <comment-list />
 </template>
 
 <script>
 import MainMenu from '@/components/MainMenu.vue';
 import TheHero from "@/components/TheHero.vue";
 import BookList from '@/components/books/BookList.vue';
+import CommentList from './components/comments/CommentList.vue';
 
 export default {
   name: "App",
   components: {
-    MainMenu, TheHero, BookList
+    MainMenu, TheHero, BookList, CommentList
   }
 }
 </script>
