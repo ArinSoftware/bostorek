03afc9e3fa3dcf7273968664a426d07a8b7a91b6
gurcancekic <arinyazilim@gmail.com>
2023-04-15 23:37:15 +0300

    router index created

---
+++ frontend/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+
+const routes = [
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/about', name: 'About', component: AboutView }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
