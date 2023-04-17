64e3ea928dad1a520ebeddb3d1a88b5d49169ab1
gurcancekic <arinyazilim@gmail.com>
2023-04-17 22:34:05 +0300

    fontawesome footer initial code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <main-menu />
   <router-view />
+  <the-footer />
 </template>
 
 <script>
 import MainMenu from '@/components/MainMenu.vue';
+import TheFooter from './components/TheFooter.vue';
 
 export default {
   name: "App",
-  components: { MainMenu }
+  components: { MainMenu, TheFooter }
 }
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,19 @@ import './assets/styles.css'
 
 import router from './router'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 app.mount('#app')
