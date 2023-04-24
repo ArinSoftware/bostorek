39bb4569ffb0f9d24216ab93be3947c9f8da877d
gurcancekic <arinyazilim@gmail.com>
2023-04-24 22:55:11 +0300

    booklist update

--- frontend/src/components/books/BookList.vue
+++ frontend/src/components/books/BookList.vue
@@ -1,16 +1,7 @@
 <template>
-  <section class="bg-white py-32 h-screen">
-    <div class="container mx-auto px-4">
-      <div class="text-center mb-16">
-        <h2 class="text-3xl font-semibold text-bost-black">
-          Latest Books
-        </h2>
-      </div>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        <book-item v-for="book in books" :key="book.id" :book="book" />
-      </div>
-    </div>
-  </section>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+    <book-item v-for="book in books" :key="book.id" :book="book" />
+  </div>
 </template>
 
 <script>
