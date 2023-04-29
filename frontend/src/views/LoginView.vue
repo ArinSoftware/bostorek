90f41f8e73ec3bd959fcf087df0b6ea4f736c9b8
gurcancekic <arinyazilim@gmail.com>
2023-04-29 22:24:35 +0300

    register page html code implemented

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,37 +1,40 @@
 <template>
-  <section class="bg-bost-grey">
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0" style="height: calc(100vh - 357px);">
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
-        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-            Sign in to your account
-          </h1>
-          <form class="space-y-4 md:space-y-6" action="#">
-            <div>
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-              <input type="email" name="email" id="email"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                placeholder="name@company.com" required="">
-            </div>
-            <div>
-              <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-              <input type="password" name="password" id="password" placeholder="••••••••"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                required="">
-            </div>
-            <button type="submit"
-              class="w-full bg-bost-green hover:bg-white text-white hover:text-bost-green border border-bost-green py-2.5 px-11 rounded-full transition duration-300 ease-in-out">Sign
-              in</button>
-          </form>
-        </div>
+  <section class="bg-bost-grey flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0"
+    style="height: calc(100vh - 357px);">
+
+    <section-header header="Login" />
+
+    <div class="w-full bg-white rounded-lg shadow-lg hover:shadow-2xl transition duration-500 md:mt-0 sm:w-1/2 lg:w-1/3">
+      <div class="p-8 space-y-4 md:space-y-6 sm:p-8">
+        <form class="space-y-8" action="#">
+          <div>
+            <input type="email" name="email" id="email"
+              class="bg-bost-grey border border-gray-300 text-gray-900 sm:text-sm rounded-full block w-full p-2.5"
+              placeholder="name@company.com" autocomplete="off">
+          </div>
+          <div>
+            <input type="password" name="password" id="password" placeholder="••••••••"
+              class="bg-bost-grey border border-gray-300 text-gray-900 sm:text-sm rounded-full block w-full p-2.5">
+          </div>
+          <button type="submit"
+            class="w-full bg-bost-green hover:bg-white text-white hover:text-bost-green border border-bost-green py-2.5 px-11 rounded-full transition duration-300 ease-in-out">
+            Sign in
+          </button>
+        </form>
       </div>
     </div>
+
   </section>
 </template>
 
 <script>
+import SectionHeader from '@/components/SectionHeader.vue';
+
 export default {
-  name: "LoginView"
+  name: "LoginView",
+  components: {
+    SectionHeader
+  }
 }
 </script>
 
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,12 +1,45 @@
 <template>
-  <div>
-    Register VIEW
-  </div>
+  <section class="bg-bost-grey flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0"
+    style="height: calc(100vh - 357px);">
+
+    <section-header header="Register" />
+
+    <div class="w-full bg-white rounded-lg shadow-lg hover:shadow-2xl transition duration-500 md:mt-0 sm:w-1/2 lg:w-1/3">
+      <div class="p-8 space-y-4 md:space-y-6 sm:p-8">
+        <form class="space-y-8" action="#">
+          <div>
+            <input type="text" name="username" id="username"
+              class="bg-bost-grey border border-gray-300 text-gray-900 sm:text-sm rounded-full block w-full p-2.5"
+              placeholder="username" autocomplete="off">
+          </div>
+          <div>
+            <input type="email" name="email" id="email"
+              class="bg-bost-grey border border-gray-300 text-gray-900 sm:text-sm rounded-full block w-full p-2.5"
+              placeholder="email@domain.com" autocomplete="off">
+          </div>
+          <div>
+            <input type="password" name="password" id="password" placeholder="••••••••"
+              class="bg-bost-grey border border-gray-300 text-gray-900 sm:text-sm rounded-full block w-full p-2.5">
+          </div>
+          <button type="submit"
+            class="w-full bg-bost-green hover:bg-white text-white hover:text-bost-green border border-bost-green py-2.5 px-11 rounded-full transition duration-300 ease-in-out">
+            Sign up
+          </button>
+        </form>
+      </div>
+    </div>
+
+  </section>
 </template>
 
 <script>
+import SectionHeader from '@/components/SectionHeader.vue';
+
 export default {
-  name: "RegisterView"
+  name: "RegisterView",
+  components: {
+    SectionHeader
+  }
 }
 </script>
 
